84136102c7722fccd58b38cbb443acea33620c5c
Giovanni Natale <johnny@johnnyxmas.net>
2016-08-24 05:17:43 -0500

    Fixed Folder Creation Issue
    *Fixed a bug that caused only the first folder to be created for a list
    of subnets, and all further subnets’ files to be placed into that same
    folder
    
    *Removed some redundant commands

--- scancannon.sh
+++ scancannon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -e "ScanCannon v0.6 by J0hnnyXm4s\n"
+echo -e "ScanCannon v0.7\n"
 
 #Help Text:
 function helptext {
@@ -30,52 +30,42 @@ fi
 #Shall we play a game?#
 #######################
 
-echo -e "Chargin' mah lazer. . .\n"
-
-#Make dir for results files if necessary:
-if [ ! -d "results" ]; then
-	echo -e "Creating parent results directory. . .\n"
-	mkdir results
-else
-	echo -e "Parent results directory exists. Continuing. . .\n"
-fi
-
 #Read in list of CIDR networks from specified file:
 for CIDR in $(cat $1); do
-	DIRNAME=$(echo $CIDR | sed -e 's/\//_/g');
 	#make results directory named after subnet, then cd into it:
-	echo "Creating results directory for $CIDR. . ."
-	mkdir -p results/$DIRNAME;
-	cd results/$DIRNAME;
+	DIRNAME=$(echo $CIDR | sed -e 's/\//_/g'); 
+	echo "Creating results directory for $CIDR. . .";
+	mkdir -p ./results/$DIRNAME;
 
 	#Start Masscan:
 	iptables -A INPUT -p tcp --dport 60000 -j DROP;
-	echo -e "\n*** Firing ScanCannon. Please keep arms and legs inside the chamber at all times ***"
-	masscan --open --banners --source-port 60000 -p0-65535 --max-rate 20000 -oB Output.bin $CIDR; masscan --readscan ./Output.bin -oL ./List-Output.txt;
+	echo -e "\n*** Firing ScanCannon. Please keep arms and legs inside the chamber at all times ***";
+	masscan --open --banners --source-port 60000 -p0-65535 --max-rate 20000 -oB ./results/$DIRNAME/masscan.bin $CIDR; masscan --readscan ./results/$DIRNAME/masscan.bin -oL ./results/$DIRNAME/masscan-output.txt;
 
-	if [ ! -s List-Output.txt ]; then
+	if [ ! -s ./results/$DIRNAME/masscan-output.txt ]; then
         	echo -e "\nNo IPs are up; skipping nmap. This was a big waste of time.\n"
 	else
 
-	#Consolidate IPs and open ports for each IP, then write to a file because files are handy:
-	awk '/open/ {print $4,$3,$2,$1}' List-Output.txt |  awk '
-    	/.+/{
-        	if (!($1 in Val)) { Key[++i] = $1; }
-        	Val[$1] = Val[$1] $2 ","; 
-    	}
-    	END{
-        	for (j = 1; j <= i; j++) {
-           		printf("%s:%s\n%s",  Key[j], Val[Key[j]], (j == i) ? "" : "\n");       
-        	}                                    
-    	}' > Up_ip_port.csv
+		#Consolidate IPs and open ports for each IP, then write to a file because files are handy:
+		awk '/open/ {print $4,$3,$2,$1}' ./results/$DIRNAME/masscan-output.txt |  awk '
+    			/.+/{
+        			if (!($1 in Val)) { Key[++i] = $1; }
+        			Val[$1] = Val[$1] $2 ","; 
+    				}
+    		END{
+        		for (j = 1; j <= i; j++) {
+          		 printf("%s:%s\n%s",  Key[j], Val[Key[j]], (j == i) ? "" : "\n");       
+        		}                                    
+    		}' > ./results/$DIRNAME/discovered.csv
 
-#Run in-depth nmap enumeration against discovered hosts & ports:
-		for TARGET in $(cat Up_ip_port.csv); do
+		#Run in-depth nmap enumeration against discovered hosts & ports:
+		for TARGET in $(cat ./results/$DIRNAME/discovered.csv); do
     	   		IP=$(echo $TARGET | awk -F: '{print $1}');
         		PORT=$(echo $TARGET | awk -F: '{print $2}' | sed 's/,$//'); #Hack to remove that pesky trailing comma
-        		FILENAME=$(echo $IP $PORT | awk '{print $1"_"$2}');
-        		nmap -vv -sV --version-intensity 5 -sT -O --max-rate 5000 -Pn -T3 -p $PORT -oA $FILENAME $IP;
+        		FILENAME=$(echo $IP | awk '{print "nmap_"$1}');
+        		nmap -vv -sV --version-intensity 5 -sT -O --max-rate 5000 -Pn -T3 -p $PORT -oA ./results/$DIRNAME/$FILENAME $IP;
 		done
 	fi
+	chmod -R 666 ./results/$DIRNAME/* #remove root-only perms 
 done
 echo -e "\nJob complete. Please check for any personal belongings before exiting the chamber."
