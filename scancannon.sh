789c268ed2457d0313498f112dcf285841460333
Giovanni Natale <johnny@johnnyxmas.net>
2016-09-06 14:22:58 -0500

    Domain and Dead Subnet Parsing
    Now generates lists of discovered domains for each subnet as well as a
    master list for all subnets
    
    Also generates list of “dead” subnets (no alive hosts)

--- scancannon.sh
+++ scancannon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -e "ScanCannon v0.7\n"
+echo -e "ScanCannon v0.8\n"
 
 #Help Text:
 function helptext {
@@ -30,6 +30,16 @@ fi
 #Shall we play a game?#
 #######################
 
+#Download and prep the lastest list of TLDs from IANA 
+if [ -s ./all_tlds.txt ]; then
+	rm ./all_tlds.txt
+	wget https://data.iana.org/TLD/tlds-alpha-by-domain.txt -O ./all_tlds.txt
+        sed -i -e '1d' all_tlds.txt -e s/^/\\[\.\]/g
+else
+	wget https://data.iana.org/TLD/tlds-alpha-by-domain.txt -O ./all_tlds.txt
+	sed -i -e '1d' all_tlds.txt -e s/^/\\[\.\]/g
+fi
+
 #Read in list of CIDR networks from specified file:
 for CIDR in $(cat $1); do
 	#make results directory named after subnet:
@@ -45,7 +55,6 @@ for CIDR in $(cat $1); do
 	if [ ! -s ./results/$DIRNAME/masscan-output.txt ]; then
         	echo -e "\nNo IPs are up; skipping nmap. This was a big waste of time.\n"
 	else
-
 		#Consolidate IPs and open ports for each IP, then write to a file because files are handy:
 		awk '/open/ {print $4,$3,$2,$1}' ./results/$DIRNAME/masscan-output.txt |  awk '
     			/.+/{
@@ -56,16 +65,31 @@ for CIDR in $(cat $1); do
         		for (j = 1; j <= i; j++) {
           		 printf("%s:%s\n%s",  Key[j], Val[Key[j]], (j == i) ? "" : "\n");       
         		}                                    
-    		}' > ./results/$DIRNAME/discovered.csv
+    		}' > ./results/$DIRNAME/discovered_hosts.csv
 
 		#Run in-depth nmap enumeration against discovered hosts & ports:
-		for TARGET in $(cat ./results/$DIRNAME/discovered.csv); do
+		for TARGET in $(cat ./results/$DIRNAME/discovered_hosts.csv); do
     	   		IP=$(echo $TARGET | awk -F: '{print $1}');
         		PORT=$(echo $TARGET | awk -F: '{print $2}' | sed 's/,$//'); #Hack to remove that pesky trailing comma
         		FILENAME=$(echo $IP | awk '{print "nmap_"$1}');
         		nmap -vv -sV --version-intensity 5 -sT -O --max-rate 5000 -Pn -T3 -p $PORT -oA ./results/$DIRNAME/$FILENAME $IP;
 		done
+	
+		#Generate list of disvoered domains for this subnet
+        	for TLD in `cat ./all_tlds.txt`; do
+                	cat ./results/$DIRNAME/$FILENAME.gnmap | egrep -i $TLD\) | awk -F[\(\)] '{print $2}' >> ./results/$DIRNAME/discovered_domains.txt;
+        	done
 	fi
-	chmod -R 666 ./results/$DIRNAME/* #remove root-only perms 
 done
+
+#Generate list subnets with no alive hosts
+comm -3 <(printf "%s\n" */*/*gnmap | sed -r 's/\/[^\/]+$//' | sort -u) <(printf "%s\n" */*) | awk -F"/" '{print $2}' | sed 's/\_/\//g' >> ./results/dead_subnets.txt
+
+#Concatenate list of all discovered domains
+for i in `find ./results -name discovered_domains.txt`; do 
+	cat $i >> ./results/all_discovered_domains.txt
+done
+
+chmod -R 777 ./results #remove file restrictions
+
 echo -e "\nJob complete. Please check for any personal belongings before exiting the chamber."
